df60d8872887317493b3af08868a5c846f7c7f08
IsmetKonan <thecodasemler@gmail.com>
2026-03-31 15:24:37 +0200

    Final Improvments

--- main.ps1
+++ main.ps1
@@ -1,6 +1,6 @@
 #
 # Made by Ismet Konan for ece24
-# Last Edited 17.03.2026
+# Last Edited 31.03.2026
 #
 
 $VERSION     = "1.0.1"
@@ -49,7 +49,7 @@ else {
             Write-Host '7-Zip ueber Fallback installiert.' -ForegroundColor Green
         }
         else {
-            Write-Host '7-Zip konnte nicht installiert werden – wird Uebersprungen.' -ForegroundColor Red
+            Write-Host 'Kritischer Fehler: 7-Zip konnte nicht installiert werden ACS' -ForegroundColor Red
         }
     }
     else {
@@ -59,8 +59,11 @@ else {
 
 $FileName = 'TeamViewer_ECE.exe'
 $FilePath = Join-Path $scriptDir $FileName
+write-Host 'Starte Download von TeamViewer...' -ForegroundColor Cyan
 Invoke-WebRequest -Uri 'https://help.ece24.net/TeamViewer_ECE.exe' -OutFile $FilePath
 Start-Process -FilePath $FilePath -ArgumentList '/S' -Wait
+Write-Host 'TeamViewer installiert!' -ForegroundColor Green
+
 
 $DownloadUrl = 'https://www.pdf-xchange.com/downloads/EditorV10.x64.msi?key=S5m2l6ycL2Imcpo00xVGGpohQ1ODS/40pyL2WXW%2Bms%2BsTE9R4X3uKziSH9gyntNU&version=10.8.4.409'
 $FileName = 'PDFXChange_Editor_Plus_10.8.4.409.msi'
@@ -68,11 +71,15 @@ $FilePath = Join-Path $scriptDir $FileName
 Write-Host 'Starte Download von PDF-XChange Editor Plus...' -ForegroundColor Cyan
 Invoke-WebRequest -Uri $DownloadUrl -OutFile $FilePath
 Start-Process 'msiexec.exe' -ArgumentList "/i `"$FilePath`" /qn /norestart" -Wait
+write-Host 'PDF-XChange Editor Plus installiert!' -ForegroundColor Green
 
+write-Host 'Starte Download von Google Chrome Enterprise...' -ForegroundColor Cyan
 $chromeUrl = 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BDD8A69D1-8F3B-4F0A-9B3C-1234567890AB%7D/chrome/install/GoogleChromeStandaloneEnterprise64.msi'
 $chromePath = Join-Path $scriptDir 'chrome-enterprise-x64.msi'
 Invoke-WebRequest -Uri $chromeUrl -OutFile $chromePath
 Start-Process 'msiexec.exe' -ArgumentList "/i `"$chromePath`" /qn /norestart" -Wait
+write-Host 'Google Chrome Enterprise installiert!' -ForegroundColor Green
+
 
 Write-Host $DEKO
 Write-Host 'All Done!' -ForegroundColor Green
