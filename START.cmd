1c4f4b2a0531df1c8f25142c3f6ef7c767bd2460
IsmetKonan <thecodasemler@gmail.com>
2026-05-26 14:53:39 +0200

    Update START.cmd to include admin execution prompt
    Added instructions to run the program as Administrator.

--- START.cmd
+++ START.cmd
@@ -2,6 +2,9 @@
 color 0A
 title init_ece
 
+echo MAKE SURE TO RUN THE PROGRAMM AS ADMINISTRATOR!
+echo trying to start main as Administrator ...
+timeout /t 2
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
  "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0main.ps1""'"
 
